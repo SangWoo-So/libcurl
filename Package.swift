7b66250e66831734dfc84ed12e426b99b3c27b8e
banaple-swso <swso@banaple.co.kr>
2024-07-05 16:40:54 +0900

    libcurl rollback ( 8.7.1 => 8.8.0 )

--- Package.swift
+++ Package.swift
@@ -16,10 +16,10 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-//        .binaryTarget(name: "libcurl", url: "https://github.com/SangWoo-So/libcurl/raw/xcframework/libcurl-xcframework-8.8.0.zip",
-//                       checksum: "4f5cf5bd403da598ca6ed3381711cfa5dd0393c29ab2d5bf571b20aef9f954f5")
+        .binaryTarget(name: "libcurl", url: "https://github.com/SangWoo-So/libcurl/raw/xcframework/libcurl-xcframework-8.8.0.zip",
+                       checksum: "4f5cf5bd403da598ca6ed3381711cfa5dd0393c29ab2d5bf571b20aef9f954f5")
         
-        .binaryTarget(name: "libcurl", url: "https://github.com/SangWoo-So/libcurl/raw/xcframework/libcurl-xcframework-8.7.1.zip",
-                       checksum: "239fadff215044ef6163dede15a14e164f427a266294ce8a0705668468b4b6f1")
+//        .binaryTarget(name: "libcurl", url: "https://github.com/SangWoo-So/libcurl/raw/xcframework/libcurl-xcframework-8.7.1.zip",
+//                       checksum: "239fadff215044ef6163dede15a14e164f427a266294ce8a0705668468b4b6f1")
     ]
 )
